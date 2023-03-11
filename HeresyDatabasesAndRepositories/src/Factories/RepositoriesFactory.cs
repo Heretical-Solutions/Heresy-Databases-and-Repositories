449d6d7195dff98924bf9d9c0448f19c1ec5684a
Benevolent Drone <benevolentdrone@gmail.com>
2023-03-11 04:56:04 +0200

    Added object repositories

--- HeresyDatabasesAndRepositories/src/Factories/RepositoriesFactory.cs
+++ HeresyDatabasesAndRepositories/src/Factories/RepositoriesFactory.cs
@@ -1,10 +1,24 @@
+using System;
 using System.Collections.Generic;
 
 namespace HereticalSolutions.Repositories.Factories
 {
 	public static partial class RepositoriesFactory
 	{
+		public static DictionaryObjectRepository BuildDictionaryObjectRepository()
+		{
+			return new DictionaryObjectRepository(
+				BuildDictionaryRepository<Type, object>());
+		}
+		
 		public static DictionaryRepository<TKey, TValue> BuildDictionaryRepository<TKey, TValue>()
+		{
+			return new DictionaryRepository<TKey, TValue>(
+				new Dictionary<TKey, TValue>());
+		}
+		
+		public static DictionaryRepository<TKey, TValue> CloneDictionaryRepository<TKey, TValue>(
+			)
 		{
 			return new DictionaryRepository<TKey, TValue>(
 				new Dictionary<TKey, TValue>());
